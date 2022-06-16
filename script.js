21f67902f84ec8281c3f329a9bb2cefb1816c756
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-06-16 15:55:55 +0100

    another commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+windows.onload = () => {
+ setTimeout(() => {
+  document.querySelector('body').classList.add('display');
+ }, 4000);
+};
+
+
+
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('change')
 });
